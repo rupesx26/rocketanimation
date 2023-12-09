d63f550cba6c90bad68fc845b421af077a7ed653
rupesx26 <rupesx26@gmail.com>
2023-12-09 23:54:49 +0530

    init commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App'
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
-
 root.render( <React.StrictMode>
     <App />
   </React.StrictMode>);
